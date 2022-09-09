e8451ef8bd735ecbdf816055451c21210f588968
kohys-dev <82067611+kohys-dev@users.noreply.github.com>
2022-09-09 23:20:08 +0900

    [level 2] Title: 튜플, Time: 0.02 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.py
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -1,8 +1,14 @@
-import re
-from collections import Counter
-
-
 def solution(s):
-    s = Counter(re.findall('\d+', s))
-    print(s)
-    return list(map(int, [k for k, v in sorted(s.items(), key=lambda x: x[1], reverse=True)]))
+    answer = []
+    sub = s.replace("},", " ").replace('{', '').replace('}', '')
+    t = sorted(sub.split(' '), key=len)
+    for item in t:
+        if ',' in item:
+            k = item.split(',')
+            for num in k:
+                if num not in answer:
+                    answer.append(num)
+        else:
+            answer.append(item)
+    return list(map(int, answer))
+

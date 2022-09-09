c0dbab3f56331d45c22a332374f9cf49513599fa
kohys-dev <82067611+kohys-dev@users.noreply.github.com>
2022-09-09 19:33:20 +0900

    [level 2] Title: 튜플, Time: 0.11 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -0,0 +1,8 @@
+import re
+from collections import Counter
+
+
+def solution(s):
+    s = Counter(re.findall('\d+', s))
+    print(s)
+    return list(map(int, [k for k, v in sorted(s.items(), key=lambda x: x[1], reverse=True)]))

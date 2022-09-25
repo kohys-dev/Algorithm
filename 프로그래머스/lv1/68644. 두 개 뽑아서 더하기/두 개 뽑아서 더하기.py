5d5ec5bd0405aaaf5ead8ee0d84485f7287d9613
kohys-dev <82067611+kohys-dev@users.noreply.github.com>
2022-09-25 23:34:48 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -4,6 +4,5 @@ def solution(numbers):
     comb_list = list(combinations(numbers, 2))
     answer = []
     for item in comb_list:
-        if sum(item) not in answer:
-            answer.append(sum(item))
-    return sorted(answer)
+        answer.append(sum(item))
+    return sorted(set(answer))
